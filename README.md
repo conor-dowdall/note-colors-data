f57c45a2aa924d1b880c309440cee5dcc01008df
Conor Dowdall <conordowdallmail@gmail.com>
2025-05-06 14:33:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Note Colors Data
+
+A lightweight package providing data structures and predefined themes for
+associating colors with musical notes. This is designed to be used in web
+applications and libraries that need to visually represent musical pitches.
---
+++ mod.ts
@@ -0,0 +1,101 @@
+/**
+ * Provides data structures and predefined themes for associating colors with musical notes.
+ * @module
+ */
+
+/**
+ * Represents a CSS color value (e.g., "#FF0000", "red", "rgba(0, 0, 0, 0.5)").
+ * Can also be `undefined` to indicate no specific color for that note in a color theme.
+ */
+export type NoteColor = string | undefined;
+
+/**
+ * An array of 12 {@link NoteColor} values, where each index
+ * corresponds to a specific pitch class in chromatic order
+ * `undefined` values are typically used in themes
+ * where only certain notes are to be assigned a color,
+ * e.g., the root note.
+ */
+export type NoteColorGroup = [
+  NoteColor,
+  NoteColor,
+  NoteColor,
+  NoteColor,
+  NoteColor,
+  NoteColor,
+  NoteColor,
+  NoteColor,
+  NoteColor,
+  NoteColor,
+  NoteColor,
+  NoteColor
+];
+
+/**
+ * Defines a color theme for musical notes.
+ */
+export interface NoteColorTheme {
+  name: string;
+  relative: boolean;
+  colors: NoteColorGroup;
+}
+
+/**
+ * A collection of predefined {@link NoteColorTheme} objects, keyed by a unique name.
+ */
+export const noteColorThemes: Record<string, NoteColorTheme> = {
+  musodojo: {
+    name: "Muso Dojo",
+    relative: false,
+    colors: [
+      "#ED2929",
+      "#9F000F",
+      "#78C7C7",
+      "#00008B",
+      "#FF9933",
+      "#EBEB19",
+      "#286704",
+      "#99CC33",
+      "#660099",
+      "#CC00FF",
+      "#BF6A1F",
+      "#FF9EE6",
+    ],
+  },
+  boomwhackers: {
+    name: "Boomwhackers",
+    relative: false,
+    colors: [
+      "#E21C48",
+      "#F26622",
+      "#F99D1C",
+      "#FFCC33",
+      "#FFF32B",
+      "#BCD85F",
+      "#62BC47",
+      "#009C95",
+      "#0071BB",
+      "#5E50A1",
+      "#8D5BA6",
+      "#CF3E96",
+    ],
+  },
+  rootIsRed: {
+    name: "Root is Red",
+    relative: true,
+    colors: [
+      "#ED2929",
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+    ],
+  },
+};
